3f3deff6e2a6958da9bf8be4b1b8abac533a314c
shridarpatil <shridharpatil2792@gmail.com>
2017-07-22 00:52:28 +0530

    Changed library name

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
+"""Setup.py"""
 from distutils.core import setup
 setup(
-  name = 'pdf_text_overlay',
-  packages = ['pdf_text_overlay'], # name of the package
-  version = '0.1',
-  description = 'Python library to write text on top of PDF',
-  author = 'Shridhar Patil',
-  author_email = 'shridharpatil2792@gmail.com',
-  url = 'https://github.com/shridarpatil/pdf_writer', #URL to the github repo
-  download_url = 'https://github.com/shridarpatil/pdf_writer/archive/0.1.tar.gz',
-  keywords = ['pdf writer', 'Pdf Editor'], # arbitrary keywords
-  classifiers = [],
-  install_requires=[
-              'pyPdf', 'reportlab'
-            ]
+    name='pdf_text_overlay',
+    packages=['pdf_text_overlay'],  # name of the package
+    version='0.1',
+    description='Python library to write text on top of PDF',
+    author='Shridhar Patil',
+    author_email='shridharpatil2792@gmail.com',
+    url='https://github.com/shridarpatil/pdf_writer',  # URL to the github repo
+    download_url='https://github.com/shridarpatil/pdf_writer/archive/0.2.tar.gz',
+    keywords=['pdf writer', 'Pdf Editor'],  # arbitrary keywords
+    classifiers=[],
+    install_requires=['pyPdf', 'reportlab']
 )
